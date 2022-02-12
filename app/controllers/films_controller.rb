d38e33f6a68551f85d9cbb7242abf8e2b7ff5af7
Smail Liams <b.smailiams@gmail.com>
2022-02-12 12:20:05 +0100

    Edit and delete actions view files

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,11 +1,13 @@
 class FilmsController < ApplicationController
 
+    before_action :find_play, only: [:show,:edit, :update, :destroy ]
+
     def index
         @plays = Film.all.order("created_at DESC")
     end
 
     def show
-        @play = Film.find(params[:id])
+        
     end
 
     def new
@@ -14,11 +16,38 @@ class FilmsController < ApplicationController
 
     def create
         @play = Film.new(film_params)
+        if @play.save
+            redirect_to root_path
+        else   
+            render 'new'
+        end
+    end
+
+    def edit
+        
+    end
+
+    def update
+        if @play.update(film_params)
+            redirect_to film_path(@play)
+        else
+            render 'edit'
+        end
+
+    end
+
+    def destroy
+        @play.destroy
+        redirect_to root_path
     end
 
     private 
 
     def film_params
-        params.require(:play).permit(:title, :description,:director)
+        params.require(:film).permit(:title, :description,:director)
+    end
+
+    def find_play
+        @play = Film.find(params[:id])
     end
 end
